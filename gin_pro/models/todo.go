72ba04ee941da9043f9e93f14ada616b8e07516f
ITU-TKG <itsukidacang41@gmail.com>
2026-04-05 08:47:55 +0900

    add calendar and date object

--- gin_pro/models/todo.go
+++ gin_pro/models/todo.go
@@ -1,10 +1,10 @@
 package models
 
 type Todo struct {
-	ID		int    `json:"id"`
-	Title	string `json:"title"`
-	Name	string `json:"name"`
-	Jender	string `json:"jender"`
-	Done	bool   `json:"done"`
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Name    string `json:"name"`
+	Jender  string `json:"jender"`
+	Done    bool   `json:"done"`
+	DueDate string `json:"due_date"`
 }
-
